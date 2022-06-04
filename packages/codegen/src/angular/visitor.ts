deb39277b645051d4a68286c70c8acffd539d59e
wassim-k <wassim.khalil9@gmail.com>
2022-06-05 09:25:16 +1000

    Added TypedDocumentNode to codegen

--- packages/codegen/src/angular/visitor.ts
+++ packages/codegen/src/angular/visitor.ts
@@ -1,25 +1,27 @@
 import { Types } from '@graphql-codegen/plugin-helpers';
-import { LoadedFragment } from '@graphql-codegen/visitor-plugin-common';
+import { getConfigValue, LoadedFragment } from '@graphql-codegen/visitor-plugin-common';
 import { GraphQLSchema, OperationDefinitionNode } from 'graphql';
 import { ApolloOrbitBaseVisitor, OperationType } from '../visitor';
 import { ApolloOrbitAngularConfig, ApolloOrbitAngularRawConfig } from './config';
 
 export class ApolloOrbitAngularVisitor extends ApolloOrbitBaseVisitor<ApolloOrbitAngularRawConfig, ApolloOrbitAngularConfig> {
-    protected supportsMutationInfo: boolean;
-    protected importPath: string;
-
     public constructor(
         schema: GraphQLSchema,
         fragments: Array<LoadedFragment>,
         rawConfig: ApolloOrbitAngularRawConfig,
-        additionalConfig: ApolloOrbitAngularConfig,
         documents?: Array<Types.DocumentFile>
     ) {
-        super(schema, fragments, rawConfig, additionalConfig, documents);
-
-        const isCore = rawConfig.importFromCore === true;
-        this.supportsMutationInfo = !isCore;
-        this.importPath = isCore ? 'angular/core' : 'angular';
+        super(
+            schema,
+            fragments,
+            rawConfig,
+            {
+                importPath: rawConfig.importFromCore === true ? '@apollo-orbit/angular/core' : '@apollo-orbit/angular',
+                supportsMutationInfo: !rawConfig.importFromCore,
+                queryObservable: getConfigValue(rawConfig.queryObservable, true)
+            } as ApolloOrbitAngularConfig,
+            documents
+        );
     }
 
     protected buildOperation(
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,4 +4,5 @@ export * from './state';
 export * from './types';
 export * from './utils/action';
 export * from './utils/array';
+export * from './utils/gql';
 export * from './utils/symbol';
