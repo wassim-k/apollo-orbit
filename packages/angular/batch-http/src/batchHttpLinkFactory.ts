e703133a429befa07e2122bc40fd408b4e71dc86
wassim-k <wassim.khalil9@gmail.com>
2024-01-30 15:31:18 +1100

    Add BatchHttpLink

--- packages/angular/batch-http/src/batchHttpLinkFactory.ts
+++ packages/angular/batch-http/src/batchHttpLinkFactory.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { makeFetch } from '@apollo-orbit/angular/fetch';
-import { HttpLink, HttpOptions } from '@apollo/client/link/http';
+import { BatchHttpLink } from '@apollo/client/link/batch-http';
 
 @Injectable()
-export class HttpLinkFactory {
+export class BatchHttpLinkFactory {
     public constructor(
         private readonly httpClient: HttpClient
     ) { }
 
-    public create(options: HttpOptions = {}): HttpLink {
-        return new HttpLink({
+    public create(options: BatchHttpLink.Options = {}): BatchHttpLink {
+        return new BatchHttpLink({
             ...options,
             fetch: makeFetch(this.httpClient)
         });
---
+++ packages/angular/batch-http/src/module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { BatchHttpLinkFactory } from './batchHttpLinkFactory';
+
+@NgModule({
+  providers: [BatchHttpLinkFactory]
+})
+export class BatchHttpLinkModule { }
